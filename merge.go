46bce792ed9c607ddbc734f35fd4bb59bdced4a8
Charles Vandevoorde <charles.vandevoorde@hotmail.be>
2018-05-22 21:33:44 +0200

    run gofmt

--- merge.go
+++ merge.go
@@ -4,20 +4,20 @@
 package go_three_way_merge
 
 import (
-    "strings"
-    "errors"
+	"errors"
+	"strings"
 
-    "github.com/sergi/go-diff/diffmatchpatch"
+	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
 const (
-    diffDelete = int(diffmatchpatch.DiffDelete)
-    diffInsert = int(diffmatchpatch.DiffInsert)
-    diffEqual = int(diffmatchpatch.DiffEqual)
+	diffDelete = int(diffmatchpatch.DiffDelete)
+	diffInsert = int(diffmatchpatch.DiffInsert)
+	diffEqual  = int(diffmatchpatch.DiffEqual)
 )
 
 var (
-    inconsistencyErr error = errors.New("Inconsistency error")
+	inconsistencyErr error = errors.New("Inconsistency error")
 )
 
 // Merge two versions of the same base file. Returns the resulting merged
@@ -25,91 +25,91 @@ var (
 // a merge-conflict or an error. In theory, this algorithm should never
 // return an error (if so, feel free to open an issue).
 func Merge(base, versionA, versionB string) (string, bool, error) {
-    result, ok, err := MergeRunes([]rune(base), []rune(versionA), []rune(versionB))
-    return result, ok, err
+	result, ok, err := MergeRunes([]rune(base), []rune(versionA), []rune(versionB))
+	return result, ok, err
 }
 
 // Same as `Merge` with runes instead.
 func MergeRunes(base, versionA, versionB []rune) (string, bool, error) {
-    dmp := diffmatchpatch.New()
+	dmp := diffmatchpatch.New()
 
-    diffA := dmp.DiffMainRunes(base, versionA, false)
-    diffIteratorA := newDiffIterator(diffA)
-    diffB := dmp.DiffMainRunes(base, versionB, false)
-    diffIteratorB := newDiffIterator(diffB)
+	diffA := dmp.DiffMainRunes(base, versionA, false)
+	diffIteratorA := newDiffIterator(diffA)
+	diffB := dmp.DiffMainRunes(base, versionB, false)
+	diffIteratorB := newDiffIterator(diffB)
 
-    var result strings.Builder
-    offsetBase := 0
+	var result strings.Builder
+	offsetBase := 0
 
-    for true {
-        if diffIteratorA.isFinished() && diffIteratorB.isFinished() {
-            break
-        } else if diffIteratorA.isFinished() || diffIteratorB.isFinished() {
-            var nonFinished *diffIterator
-            if diffIteratorA.isFinished() {
-                nonFinished = &diffIteratorB
-            } else {
-                nonFinished = &diffIteratorA
-            }
+	for true {
+		if diffIteratorA.isFinished() && diffIteratorB.isFinished() {
+			break
+		} else if diffIteratorA.isFinished() || diffIteratorB.isFinished() {
+			var nonFinished *diffIterator
+			if diffIteratorA.isFinished() {
+				nonFinished = &diffIteratorB
+			} else {
+				nonFinished = &diffIteratorA
+			}
 
-            // TODO: verify this is always correct.
-            if nonFinished.diffType() != diffInsert {
-                return result.String(), false, errors.New("Unexpected diff.")
-            }
-            result.WriteRune(nonFinished.valueRune())
-            nonFinished.next()
-            continue
-        }
+			// TODO: verify this is always correct.
+			if nonFinished.diffType() != diffInsert {
+				return result.String(), false, errors.New("Unexpected diff.")
+			}
+			result.WriteRune(nonFinished.valueRune())
+			nonFinished.next()
+			continue
+		}
 
-        aType, bType := diffIteratorA.diffType(), diffIteratorB.diffType()
-        aValue, bValue := diffIteratorA.valueRune(), diffIteratorB.valueRune()
-        baseValue := base[offsetBase]
+		aType, bType := diffIteratorA.diffType(), diffIteratorB.diffType()
+		aValue, bValue := diffIteratorA.valueRune(), diffIteratorB.valueRune()
+		baseValue := base[offsetBase]
 
-        if aType == diffEqual && bType == diffEqual {
-            if aValue != baseValue || bValue != baseValue {
-                return result.String(), false, inconsistencyErr
-            }
-            diffIteratorA.next()
-            diffIteratorB.next()
-            offsetBase += 1
+		if aType == diffEqual && bType == diffEqual {
+			if aValue != baseValue || bValue != baseValue {
+				return result.String(), false, inconsistencyErr
+			}
+			diffIteratorA.next()
+			diffIteratorB.next()
+			offsetBase += 1
 
-            result.WriteRune(baseValue)
-        } else if aType != diffEqual && bType != diffEqual {
-            // Possible merge conflict...
+			result.WriteRune(baseValue)
+		} else if aType != diffEqual && bType != diffEqual {
+			// Possible merge conflict...
 
-            if aValue == bValue && aType == bType {
-                // Same diff so doesn't conflict.
-                diffIteratorA.next()
-                diffIteratorB.next()
+			if aValue == bValue && aType == bType {
+				// Same diff so doesn't conflict.
+				diffIteratorA.next()
+				diffIteratorB.next()
 
-                if aType == diffInsert {
-                    result.WriteRune(aValue)
-                } else if aType == diffDelete {
-                    offsetBase += 1
-                }
-            } else {
-                // A merge-conflict has been found.
-                return result.String(), false, nil
-            }
-        } else if aType == diffDelete || bType == diffDelete {
-            if aValue != baseValue || bValue != baseValue {
-                return result.String(), false, inconsistencyErr
-            }
-            diffIteratorA.next()
-            diffIteratorB.next()
-            offsetBase += 1
-        } else if aType == diffInsert || bType == diffInsert {
-            var insertIterator *diffIterator
-            if aType == diffInsert {
-                insertIterator = &diffIteratorA
-            } else {
-                insertIterator = &diffIteratorB
-            }
+				if aType == diffInsert {
+					result.WriteRune(aValue)
+				} else if aType == diffDelete {
+					offsetBase += 1
+				}
+			} else {
+				// A merge-conflict has been found.
+				return result.String(), false, nil
+			}
+		} else if aType == diffDelete || bType == diffDelete {
+			if aValue != baseValue || bValue != baseValue {
+				return result.String(), false, inconsistencyErr
+			}
+			diffIteratorA.next()
+			diffIteratorB.next()
+			offsetBase += 1
+		} else if aType == diffInsert || bType == diffInsert {
+			var insertIterator *diffIterator
+			if aType == diffInsert {
+				insertIterator = &diffIteratorA
+			} else {
+				insertIterator = &diffIteratorB
+			}
 
-            result.WriteRune(insertIterator.valueRune())
-            insertIterator.next()
-        }
-    }
+			result.WriteRune(insertIterator.valueRune())
+			insertIterator.next()
+		}
+	}
 
-    return result.String(), true, nil
+	return result.String(), true, nil
 }
